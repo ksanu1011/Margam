a8d72bdd3041863fd72861773424145dccf5ddd6
ksanu1011 <kumarsanu11nov04@gmail.com>
2025-12-08 15:18:57 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://margam-ptd1.onrender.com/api', // Backend URL
+    baseURL: import.meta.env.VITE_API_URL || 'https://margam-ptd1.onrender.com', // Backend URL
     headers: {
         'Content-Type': 'application/json',
     },
